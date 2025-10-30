a68e986010ef7f5017e042ea193ab042dffa0c07
jowgaze <jonatha.biomon.hp@gmail.com>
2025-10-29 22:08:08 -0300

    atualiza querys de repositories

--- src/main/java/com/challenge/repositories/ProductSaleRepository.java
+++ src/main/java/com/challenge/repositories/ProductSaleRepository.java
@@ -2,6 +2,19 @@ package com.challenge.repositories;
 
 import com.challenge.domain.model.ProductSale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductSaleRepository extends JpaRepository<ProductSale, Long> {
+    @Query("""
+            SELECT p.name AS name, SUM(ps.quantity) AS totalSold, SUM(ps.totalPrice) AS totalRevenue,
+            RANK() OVER (ORDER BY SUM(ps.quantity) DESC) AS position
+            FROM ProductSale ps JOIN Product p ON p.id = ps.product.id JOIN Sale s ON s.id = ps.sale.id
+            WHERE s.saleStatusDesc = 'COMPLETED'
+            AND (:channelId IS NULL OR s.channel.id = :channelId)
+            GROUP BY p.name ORDER BY totalSold DESC LIMIT 10
+            """)
+    List<Object[]> findTopProduct(@Param("channelId") Long channelId);
 }
--- src/main/java/com/challenge/repositories/SaleRepository.java
+++ src/main/java/com/challenge/repositories/SaleRepository.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
-    @Query("SELECT SUM(s.totalAmountItems) FROM Sale s WHERE s.channel.type = :type AND s.createdAt BETWEEN :start AND :end")
-    BigDecimal findTotalSales(@Param("type") String type, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+    @Query("SELECT SUM(s.totalAmountItems) FROM Sale s WHERE s.channel.type = :channelType AND s.createdAt BETWEEN :start AND :end")
+    BigDecimal findTotalSales(@Param("channelType") String channelType, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
 }
